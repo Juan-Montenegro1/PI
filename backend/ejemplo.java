5f3511f8f9d7c66bde6cd8e2798cd1051227b77f
Juan-montenegro1 <Juan.montenegro01@uceva.edu.co>
2025-02-21 16:37:49 -0500

    .

--- backend/ejemplo.java
+++ backend/ejemplo.java
@@ -3,5 +3,6 @@ package backend;
 public class ejemplo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Opciones:"+ "\n" + "1. ");
     }
 }
